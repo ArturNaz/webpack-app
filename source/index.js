c4fc63b48b02d19bba29362b7cefc2c4c48f3a0b
ArturNaz <exbayr4@mail.ru>
2018-12-16 11:06:42 +0400

    watch

--- source/index.js
+++ source/index.js
@@ -1,4 +1,5 @@
 import  createMenu from './menu';
 
-let menu = createMenu()(['Main','About me','Portfolio'],'menu');
+let menu = createMenu(['Main menu','About me','Portfolio'],'menu');
+console.log(menu);
 document.body.appendChild(menu);
--- source/menu.js
+++ source/menu.js
@@ -2,7 +2,7 @@ export default function (array, className) {
     let menu = document.createElement("ul");
     menu.className = className;
     let listItems = '';
-    array.forEach((item) => {
+    array.forEach(function(item) {
         listItems += '<li>' + item + '</li>';
     });
     menu.innerHTML = listItems;
